edb33746b4c6fbc3fb53d244c0dd1e4c87cda102
Tong Zhou <tongzhou@Tongs-MacBook-Pro.local>
2020-03-01 14:18:26 -0800

    Updates

---
+++ precache-manifest.041a5f758115be83969e0bc4ec5748e4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dede1b6ad248d2c834b10c667e949b95",
+    "url": "/hello/index.html"
+  },
+  {
+    "revision": "1c39d3bce3c28b6234e0",
+    "url": "/hello/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "991cfd0e930107752593",
+    "url": "/hello/static/js/2.83d3a432.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/hello/static/js/2.83d3a432.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c39d3bce3c28b6234e0",
+    "url": "/hello/static/js/main.1c2f159e.chunk.js"
+  },
+  {
+    "revision": "aa39a84bed07968e8594",
+    "url": "/hello/static/js/runtime-main.3283f4f9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/hello/static/media/logo.5d5d9eef.svg"
+  }
+]);
